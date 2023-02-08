edc3780fd7155552f251e3458b81399add2f9213
felix medina <felixjmedinam@gmail.com>
2023-02-07 22:14:29 -0400

    fix: new changes

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Altura } from "@altura/altura-js";
+// import { Altura } from "@altura/altura-js";
 import { Layout } from "../components/Layout";
 import PrincipalScreen from "../components/PrincipalScreen";
 import SecundaryScreen from "../components/SecundaryScreen";
@@ -13,21 +13,21 @@ const Divider = styled("div")({
     "linear-gradient(90deg, rgba(159, 74, 255, 0.1) 0%, rgba(169, 157, 255, 0.4) 50%, rgba(159, 74, 255, 0.1) 100%);",
 });
 const Login = () => {
-  const handleClickNew = async () => {
-    const newVar = new Altura("D6RSE4R-9T6MJ8H-K1HN3PQ-5ZHRFYT");
-    const alturaUser = await newVar.getUser(
-      "0xcdec817e160fea97438e7d777ca48b8f41a4cd02"
-    );
-    console.log("hola");
-    try {
-      console.log(alturaUser);
-    } catch (e) {
-      console.log(e.message);
-    }
-  };
+  // const handleClickNew = async () => {
+  //   const newVar = new Altura("D6RSE4R-9T6MJ8H-K1HN3PQ-5ZHRFYT");
+  //   const alturaUser = await newVar.getUser(
+  //     "0xcdec817e160fea97438e7d777ca48b8f41a4cd02"
+  //   );
+  //   console.log("hola");
+  //   try {
+  //     console.log(alturaUser);
+  //   } catch (e) {
+  //     console.log(e.message);
+  //   }
+  // };
   return (
     <>
-      <div onClick={() => handleClickNew()}></div>
+      <div></div>
       <Layout>
         <PrincipalScreen />
         <SecundaryScreen />
